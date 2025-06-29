91c474d0a9854201a7a9cd48e34229f6545c962a
Edil Ashimov <eddyashimov@gmail.com>
2025-06-29 07:34:47 -0400

    Remove build/cache files from Git and update .gitignore

---
+++ shared/src/commonMain/kotlin/com/plcoding/translator_kmm/translate/data/translate/TranslateDTO.kt
@@ -0,0 +1,9 @@
+package com.plcoding.translator_kmm.translate.data.translate
+import kotlinx.serialization.SerialName
+
+@kotlinx.serialization.Serializable
+data class TranslateDTO(
+    @SerialName("q") val textToTranslate: String,
+    @SerialName("source") val sourceLanguageCode: String,
+    @SerialName("target") val targetLanguageCode: String
+)
